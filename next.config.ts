0c2cb41d21cd0c79f90d9a3095101bb1f5c05b61
SAAD-Qf <159832153+SAAD-Qf@users.noreply.github.com>
2025-01-26 02:20:14 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // Enable React's strict mode for better debugging
-  swcMinify: true, // Enable SWC-based minification for faster builds
   eslint: {
-    ignoreDuringBuilds: true, // Ignore ESLint errors during builds
-  },
-  typescript: {
-    ignoreBuildErrors: true, // Ignore TypeScript errors during builds (optional)
+    ignoreDuringBuilds: true,
   },
 };
 
