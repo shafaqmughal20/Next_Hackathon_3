8255fb2e4f38ef49aafca4db2451d7d8ecd34216
ShafaqMughal <shafaqaslam09@gmail.com>
2025-01-26 12:29:53 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/app/Footer/Footer";
 
 
 
-
+// abcdrfghijklmnop
 const font = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
   subsets: ["latin"],
