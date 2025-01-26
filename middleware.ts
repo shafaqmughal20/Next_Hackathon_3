c45c3b614e7c52e1f42c03ab59749f429698b5fd
SAAD-Qf <159832153+SAAD-Qf@users.noreply.github.com>
2025-01-26 02:21:03 -0800

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(req: NextRequest) {
-  // Disable draft mode
-  if (req.nextUrl.pathname.includes("_next/data")) {
-    req.headers.delete("x-vercel-draft-mode");
-  }
-  return NextResponse.next();
-}
